9bd6bf77895044d332442d2db3fda92f511be5f3
andrew.boren <andrew.boren@gmail.com>
2011-03-01 21:16:37 -0700

    supersonic: Remove non-evervolv build options.
    Change-Id: I944fbcfdc99f2b13fe0db18a0fb0c288afb7d2f9

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo htc_supersonic-userdebug
-add_lunch_combo htc_supersonic-eng
+#We're only going to be building Evervolv, so comment this out for now
+#add_lunch_combo htc_supersonic-userdebug
+#add_lunch_combo htc_supersonic-eng
