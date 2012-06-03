d3c535baea07334741112b38c378ef0e4f799d13
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-06-02 21:17:15 -0500

    supersonic: use qsd8k common
    Change-Id: I66b782d8daf223380d4ff349ca9b1d9445cf7ad1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,87 +31,14 @@ BOARD_HAVE_HTC_FFC := true
 
 # inherit from the proprietary version
 -include vendor/htc/supersonic/BoardConfigVendor.mk
-
-TARGET_NO_BOOTLOADER := true
-
-TARGET_BOARD_PLATFORM := qsd8k
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
+# inherit common defines for all qsd8k devices
+include device/htc/qsd8k-common/BoardConfigCommon.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := supersonic
-TARGET_SPECIFIC_HEADER_PATH := device/htc/supersonic/include
-
-# Wifi related defines
-BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WLAN_DEVICE                := bcm4329
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
-WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/firmware/fw_bcm4329_apsta.bin"
-WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan firmware_path=/system/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
-WIFI_DRIVER_MODULE_NAME          := "bcm4329"
-
-BOARD_USES_GENERIC_AUDIO := false
-# prevent breakage from QCOM_HARDWARE in system/audio.h
-COMMON_GLOBAL_CFLAGS += -DLEGACY_AUDIO_COMPAT
 
 BOARD_KERNEL_CMDLINE := no_console_suspend=1
 BOARD_KERNEL_BASE := 0x20000000
 BOARD_KERNEL_NEW_PPPOX := true
-
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-
-BOARD_VENDOR_QCOM_AMSS_VERSION := 3200
-
-BOARD_VENDOR_USE_AKMD := akm8973
-
-# Hardware rendering
-BOARD_EGL_CFG           := device/htc/supersonic/prebuilt/lib/egl/egl.cfg
-USE_OPENGL_RENDERER     := true
-TARGET_USES_GENLOCK     := true
-# Unneccesary with new egl libs
-#COMMON_GLOBAL_CFLAGS   += -DMISSING_EGL_EXTERNAL_IMAGE -DMISSING_EGL_PIXEL_FORMAT_YV12
-# We only have 2 buffers so still neccesary to hack it.
-COMMON_GLOBAL_CFLAGS    += -DMISSING_GRALLOC_BUFFERS #-DFORCE_EGL_CONFIG=0x9
-# Unneccesary. Just a safety measure to make sure its all included
-COMMON_GLOBAL_CFLAGS    += -DQCOM_HARDWARE
-# Force refresh rate since fps calc is broke and reports 0
-COMMON_GLOBAL_CFLAGS    += -DREFRESH_RATE=60
-# qsd dont have overlay
-TARGET_USE_OVERLAY      := false
-# qsd dont have bypass
-TARGET_HAVE_BYPASS      := false
-# qsd dont support c2d
-TARGET_USES_C2D_COMPOSITION := false
-
-# Try to use ASHMEM if possible (when non-MDP composition is used)
-# if enabled, set debug.sf.hw=1 in system.prop
-# This is still confusing to me disabling for now since pmem and mdp seems to work fine
-#TARGET_GRALLOC_USES_ASHMEM := true
-
-# Find out what these do..if anything
-# used in cafs tree nothing actually present is ours (yet)
-#HAVE_ADRENO200_SOURCE := true
-#HAVE_ADRENO200_SC_SOURCE := true
-#HAVE_ADRENO200_FIRMWARE := true
-#BOARD_USES_QCNE := true
-# I dont think these do anything but everyone else is using them
-#BOARD_USE_QCOM_PMEM := true
-#BOARD_USES_ADRENO_200 := true
-#TARGET_HARDWARE_3D := false
-# Debuging egl
-COMMON_GLOBAL_CFLAGS += -DEGL_TRACE #-DDEBUG_CALC_FPS
-
-TARGET_FORCE_CPU_UPLOAD  := true
-BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QCOM_LIBS := true
-#BOARD_USES_LEGACY_QCOM := true
 
 BOARD_USE_OPENSSL_ENGINE := true
 
@@ -120,9 +47,6 @@ BOARD_USE_REVERSE_FFC := true
 
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := supersonic
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
-
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 
 BOARD_HAVE_SQN_WIMAX := true
 
@@ -149,8 +73,5 @@ BOARD_HAS_NO_SELECT_BUTTON := 1
 
 #TARGET_RECOVERY_UI_LIB := librecovery_ui_supersonic librecovery_ui_htc
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
-
 TARGET_KERNEL_CONFIG    := evervolv_supersonic_defconfig
-TARGET_KERNEL_SOURCE    := kernel/htc/qsd8k
 TARGET_PREBUILT_KERNEL  := device/htc/supersonic/prebuilt/root/kernel
