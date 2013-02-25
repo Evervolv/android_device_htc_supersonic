edb800429e138e8b3ae58e37b682c318db8dafc4
andrew.boren <andrew.boren@gmail.com>
2013-02-24 17:47:17 -0700

    supersonic: add fstab to mount /data to mmcblk0p2
    Change-Id: Id79eaa2d2d64b3f878c37eaf6386853bc29c5fff

--- full_supersonic.mk
+++ full_supersonic.mk
@@ -26,6 +26,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/supersonic/prebuilt/root/init.supersonic.rc:root/init.supersonic.rc \
     device/htc/supersonic/prebuilt/root/init.supersonic.usb.rc:root/init.supersonic.usb.rc \
+    device/htc/supersonic/fstab.supersonic:root/fstab.supersonic \
     device/htc/supersonic/prebuilt/root/ueventd.supersonic.rc:root/ueventd.supersonic.rc 
 
 $(call inherit-product-if-exists, vendor/htc/supersonic/supersonic-vendor.mk)
